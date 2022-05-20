91fdaa6e9f02bc359f5643e21a889134d25a95ad
renanrith <renaneuemesmo@gmail.com>
2022-05-19 23:34:52 -0300

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import PaginaInicial from "./paginas/paginaInicial";
+import PaginaLogin from "./paginas/paginaLogin";
 
 function App() {
   return (
     <div>
       <Routes>
-      <Route path="home" element={<PaginaInicial />} />
+        <Route path="home" element={<PaginaInicial />} />
+        <Route path="/" element={<PaginaLogin />} />
       </Routes>
     </div>
   );
--- src/paginas/paginaLogin.js
+++ src/paginas/paginaLogin.js
@@ -1,27 +1,29 @@
-import classes from "./paginaLogin.css";
 import logo_sem_titulo from "../imagens/MainNavigation/logo.svg";
 import { Link } from "react-router-dom";
+import "./paginaLogin.css";
 
 export default function  PaginaLogin(){
     return (
-        <div className={classes.wrapper}>
-        <header className={classes.header}>
-        <div className={classes.cadastro}>
+        <div style = {{height:"100vh"}} className="ola">
+        <div className="wrapper">
+        <div className="header">
+        <div className="cadastro">
             <Link to="/home"> <button > Cadastre-se </button> </Link>
         </div>
-      </header>
+      </div>
 
-        <div className={classes.login}>
+        <div className="login">
             <img src={logo_sem_titulo} alt='ArtRoom Logo'/> <br/>
             Seja bem vindo!!
             <form>
             <input type="text" name="username" id="username" placeholder="Username / Email"/> <br/>
             <input type="password" name="password" id="password" placeholder="Senha"/> <br/>
-            <a href="index.html"> <input type="button" value="Logar" className={classes.button}/> </a> <br/>
-            <a href="index.html"> <button className={classes.cadastro}> Cadastre-se </button> </a>
+            <a href="index.html"> <input type="button" value="Logar" className="button"/> </a> <br/>
+            <a href="index.html"> <button className="cadastro"> Cadastre-se </button> </a>
             </form>
             <p id="subtext"> Ao continuar, você concorda com os <a href="https://www.youtube.com/watch?v=o-V643L37QQ&t=621s"> termos de serviços </a> do ArtRoom e confirma que leu nossa politica de privacidade e uso de cookies  </p>
         </div>
+    </div>
     </div>
     )
 }
