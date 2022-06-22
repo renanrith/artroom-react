784e8165b9c3edd49a39ddaceb688247f5570c73
renanrith <renaneuemesmo@gmail.com>
2022-06-22 16:12:31 -0300

    Configure React app for deployment to GitHub Pages

--- src/components/layout/post/modal.js
+++ src/components/layout/post/modal.js
@@ -4,13 +4,32 @@ import audioIcon from "./../../../imagens/makepost/audIcon.png";
 import textIcon from "./../../../imagens/makepost/txtIcon.png";
 import videoIcon from "./../../../imagens/makepost/videoIcon.png";
 
-export default function PostPopup(){
-    return(
+export default function PostPopup() {
+  return (
     <div className={classes.modal}>
-    <img src={imageIcon} className={classes.icon}/>
-    <img src={audioIcon} className={classes.icon}/>
-    <img src={textIcon} className={classes.icon}/>
-    <img src={videoIcon} className={classes.icon}/>
+      <input
+        className={classes.titulo}
+        type="text"
+        name="Titulo"
+        id="Titulo"
+        placeholder="Titulo"
+        autoComplete="off"
+        autoCapitalize="On"
+      />
+      <input
+        className={classes.descricao}
+        type="text"
+        name="Descricao"
+        id="Descricao"
+        placeholder="Descrição"
+        autoComplete="off"
+        autoCapitalize="On"
+      />
+      <br/>
+      <img src={imageIcon} className={classes.icon} />
+      <img src={audioIcon} className={classes.icon} />
+      <img src={textIcon} className={classes.icon} />
+      <img src={videoIcon} className={classes.icon} />
     </div>
-    )
+  );
 }
