cde4d2763820d2f11d858a6edaa63904fa9ed77e
renanrith <renaneuemesmo@gmail.com>
2022-06-24 17:02:22 -0300

    Perfil

--- src/paginas/perfil.jsx
+++ src/paginas/perfil.jsx
@@ -1,4 +1,4 @@
-import "./perfilcss.module.css"
+import classes from "./perfilcss.module.css"
 import chirio from "./../imagens/imgperfil/chirio.jpg";
 import bell_icon from "../imagens/MainNavigation/bell_icon.png";
 import profile_icon from "../imagens/MainNavigation/profile_icon.png";
@@ -7,23 +7,46 @@ import logo_sem_titulo from "../imagens/MainNavigation/logo_sem_titulo.svg";
 import art3 from "./../imagens/imgperfil/artie3.jpg"
 import art7 from "./../imagens/imgperfil/artie7.jpg"
 import { Link } from "react-router-dom";
+import PostsAudioList from "../components/posts/postAudio/postAudioList";
+import PostsLists from "../components/posts/postImagem/posts_list";
 
 export default function Perfil(){
 
-return(
-<body>
 
+    const DUMMY_DATA = [
+        {
+          id: "m1",
+          user: "Rafla",
+          user_image:
+            " http://localhost:3000/artroom-react/static/media/chirio.90c6d6f99ced9fb94811.jpg ",
+          title: "Artie",
+          image: "https://pbs.twimg.com/media/FWCu0t7X0AAb6ID?format=jpg&name=large",
+          description: "eu adoro o artie",
+        },
+        {
+            id: "m2",
+            user: "Rafla",
+            user_image:
+              " http://localhost:3000/artroom-react/static/media/chirio.90c6d6f99ced9fb94811.jpg ",
+            title: "Artie",
+            image: "https://pbs.twimg.com/media/FWCvYzSWAAERs6S?format=jpg&name=4096x4096",
+            description: "eu adoro o artie",
+        },
+      ];
+
+return(
+<div>
     <header>
-        <div class="icons">
-            <img className="icone" src={bell_icon} alt="notifications"/>
-            <img className="icone" src={profile_icon} alt="perfil"/>
-            <img className="icone" src={Marketplace_Icon} alt="marketplace"/>
+        <div className={classes.icons}>
+            <img className={classes.icone} src={bell_icon} alt="notifications"/>
+            <img className={classes.icone} src={profile_icon} alt="perfil"/>
+            <img className={classes.icone} src={Marketplace_Icon} alt="marketplace"/>
         </div>
 
-        <div class="direita">
+        <div className={classes.direita}>
             <input type="text" />
             <Link to="/home">
-            <img class="iconeart" src={logo_sem_titulo} alt="iconeart"/>
+            <img className={classes.iconeart} src={logo_sem_titulo} alt="iconeart"/>
             </Link>
         </div>
 
@@ -32,10 +55,10 @@ return(
 
     <div>
 
-        <div class="card-container">
-            <div class="top">
+        <div className={classes.cardContainer}>
+            <div className={classes.top}>
 
-                <div class="img-header">
+                <div className={classes.imgHeader}>
                     <img src={chirio}/>
                 </div>
 
@@ -45,37 +68,15 @@ return(
 
     </div>
 
-    <div class="lateral-meio bottom">
+    <div className={classes.bottom}>
         <h3>Rafaela</h3>
         <h4>Designer Digital</h4>
         <p>Gosto muito de pintura digital e estou começando <br/> agr em 3D, me desejem sorte! ^^ </p>
-        <a href="#" class="btn">Chat</a>
+        <a href="#" className={classes.btn}>Chat</a>
 
-        <div class="posts">
-            <div class="post">
-                <h5> rafaela oi</h5>
-                <img src={art3}/>
-                <h5> eu adoro o artie</h5>
-                
-            </div>
-
-            <div class="post">
-                <h5> rafaela oi</h5>
-                <img src={art7}/>
-                <h5> eu adoro o artie</h5>
-            </div>
-        </div>
+    <PostsLists postsImages={DUMMY_DATA}/>
 
     </div>
-
-
-    <footer>
-
-
-
-
-    </footer>
-
-</body>
+</div>
 )
 }
