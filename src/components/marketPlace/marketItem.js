72581b18206c493fe58f6c195f38fb4cbc79685c
renanrith <renaneuemesmo@gmail.com>
2022-09-29 19:31:57 -0300

    market

--- src/components/marketPlace/marketItem.js
+++ src/components/marketPlace/marketItem.js
@@ -2,19 +2,19 @@ import classes from "./marketItem.module.css";
 import Tilt from 'react-parallax-tilt';
 
 export default function MarketItem(props) {
-return(
-<div  className={classes.container}>
-    <Tilt>
-    <div className={classes.card}>
-      <div className={classes.content}>
-        <img src={props.image} alt="Imagem" />
-        <h2>{props.title}</h2>
-        <h2>{props.preco} R$</h2>
-        <p>{props.description}</p>
-        <a>Ver mais</a>
-      </div>
+  return (
+    <div className={classes.container}>
+      <Tilt>
+        <div className={classes.card}>
+          <div className={classes.content}>
+            <img src={props.image} alt="Imagem" />
+            <h2>{props.title}</h2>
+            <h2>{props.preco} R$</h2>
+            <p>{props.description}</p>
+            <a>Ver mais</a>
+          </div>
+        </div>
+      </Tilt>
     </div>
-    </Tilt>
-  </div>
-)
+  )
 }
