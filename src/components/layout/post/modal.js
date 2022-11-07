71115a035cfaf2267f460a05eef4a7be77030fd1
Blood Sorceress <83647639+scarletsorceress@users.noreply.github.com>
2022-11-07 15:06:46 -0300

    post imagem

--- src/components/layout/post/modal.js
+++ src/components/layout/post/modal.js
@@ -1,24 +1,43 @@
 import classes from "./Modal.module.css";
 import Axios from "axios";
-import { useState } from "react";
+import { supabase } from "../../../supabase/supabaseClient";
+import { useState, useRef } from "react";
 import { MdAddPhotoAlternate } from "react-icons/md"
 import { AiFillSound } from "react-icons/ai"
-import { RiVideoAddFill } from "react-icons/ri"
+import { RiVideoAddFill } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 export default function PostPopup(props){
   const [titulo, setTitulo] = useState("");
   const [descricao, setDescricao] = useState("");
+  const [fileUploaded, setFileUploaded] = useState(null);
+  const navigate = useNavigate();
 
-  const handleSubmit = () => {
+
+  const hiddenFileInput = useRef(null);
+
+  const handleSubmit = async (event) => {
+    const fileExt = fileUploaded.type.split('.')[1];
+    const fileName = `${Date.now()}.${fileExt}.png`;
+    const { data, error } = await supabase.storage
+    .from('images')
+    .upload(`public/${fileName}`, fileUploaded);
+
+    const { data: publicURL, error: publicURLError } = await supabase.storage
+    .from('images')
+    .getPublicUrl(`public/${fileName}`)
+    console.log(publicURL.publicURL)
+
     Axios.post("http://localhost:8080/upload/uploads", {
       titulo: titulo,
       descricao: descricao,
       author: localStorage.getItem("username"),
-      type : "text",
+      type : "image",
+      image: publicURL.publicURL.split('"'),
       like: 0
     });
-    window.location.reload()
-  }
+    navigate("/home");
+  };
 
     return(
         <div className={classes.modal}>
@@ -44,6 +63,7 @@ export default function PostPopup(props){
           onChange={(e) => { setDescricao(e.target.value)} }
         />
         <br/>
+        <input type="file" accept=".png" ref={hiddenFileInput} onChange={(e) => { setFileUploaded(e.target.files[0]) }}/>
         <MdAddPhotoAlternate size={40} className={classes.icon} color="#31F0F7"/>
         <AiFillSound size={40} className={classes.icon} color="#F72F3A"/>
         <RiVideoAddFill size={40} className={classes.icon} color="#6C3BF4"/>
